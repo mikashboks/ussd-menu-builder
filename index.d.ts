c0683107c9088107291fb27b48e5693bb5842f23
Salton Massally <salton.massally@gmail.com>
2023-04-13 21:07:39 -0400

    add operator to args

--- index.d.ts
+++ index.d.ts
@@ -9,116 +9,127 @@ import { EventEmitter } from "events";
 export = UssdMenu;
 
 declare class UssdState {
-    constructor(menu: UssdMenu);
+  constructor(menu: UssdMenu);
 
-    defaultNext?: string;
+  defaultNext?: string;
 
-    menu: UssdMenu;
+  menu: UssdMenu;
 
-    name: string;
+  name: string;
 
-    run(): void;
+  run(): void;
 
-    val: string;
+  val: string;
 }
 
 declare class UssdMenu extends EventEmitter {
-    constructor(opts?: UssdMenu.UssdMenuOptions);
+  constructor(opts?: UssdMenu.UssdMenuOptions);
 
-    session: any;
-    provider: UssdMenu.UssdMenuProvider;
-    args: UssdMenu.UssdGatewayArgs;
-    states: Array<UssdState>;
-    result: string;
-    val: string;
+  session: any;
+  provider: UssdMenu.UssdMenuProvider;
+  args: UssdMenu.UssdGatewayArgs;
+  states: Array<UssdState>;
+  result: string;
+  val: string;
 
-    callOnResult(): void;
+  callOnResult(): void;
 
-    con(text: string): void;
+  con(text: string): void;
 
-    end(text: string): void;
+  end(text: string): void;
 
-    getRoute(args: UssdMenu.UssdGatewayArgs | UssdMenu.HubtelArgs): Promise<string>;
+  getRoute(
+    args: UssdMenu.UssdGatewayArgs | UssdMenu.HubtelArgs
+  ): Promise<string>;
 
-    go(state: string): void;
+  go(state: string): void;
 
-    goStart(): void;
+  goStart(): void;
 
-    mapArgs(args: UssdMenu.UssdGatewayArgs | UssdMenu.HubtelArgs): UssdMenu.UssdGatewayArgs;
+  mapArgs(
+    args: UssdMenu.UssdGatewayArgs | UssdMenu.HubtelArgs
+  ): UssdMenu.UssdGatewayArgs;
 
+  onResult?(result: string | UssdMenu.HubtelResponse): void;
 
-    onResult?(result: string | UssdMenu.HubtelResponse): void;
+  resolveRoute(route: string, callback: Function): void;
 
-    resolveRoute(route: string, callback: Function): void;
+  resolve?(value: string): void;
 
-    resolve?(value: string): void;
+  run(args: UssdMenu.UssdGatewayArgs, onResult?: Function): Promise<string>;
 
-    run(args: UssdMenu.UssdGatewayArgs, onResult?: Function): Promise<string>;
+  runState(state: UssdState): void;
 
-    runState(state: UssdState): void;
+  sessionConfig(config: UssdMenu.UssdSessionConfig): void;
 
-    sessionConfig(config: UssdMenu.UssdSessionConfig): void;
+  startState(options: UssdMenu.UssdStateOptions): void;
 
-    startState(options: UssdMenu.UssdStateOptions): void;
+  state(name: string, options: UssdMenu.UssdStateOptions): UssdMenu;
 
-    state(name: string, options: UssdMenu.UssdStateOptions): UssdMenu;
+  testLinkRule(rule: string, val: string): boolean;
 
-    testLinkRule(rule: string, val: string): boolean;
-
-    static START_STATE: string;
+  static START_STATE: string;
 }
 
 /*~ If you want to expose types from your module as well, you can
  *~ place them in this block.
  */
 declare namespace UssdMenu {
-    interface NextState {
-        [state: string]: Function | string;
-    }
+  interface NextState {
+    [state: string]: Function | string;
+  }
 
-    interface UssdGatewayArgs {
-        text: string;
-        phoneNumber: string;
-        sessionId: string;
-        serviceCode: string;
-    }
+  interface UssdGatewayArgs {
+    text: string;
+    phoneNumber: string;
+    sessionId: string;
+    serviceCode: string;
+    Operator: string;
+  }
 
-    interface HubtelResponse {
-        Type: 'Response' | 'Release';
-        Message: string;
-    }
+  interface HubtelResponse {
+    Type: "Response" | "Release";
+    Message: string;
+  }
 
-    interface HubtelArgs {
-        Mobile: string;
-        SessionId: string;
-        ServiceCode: string;
-        Type: 'Initiation' | 'Response' | 'Release' | 'Timeout';
-        Message: string;
-        Operator: 'Tigo' | 'Airtel' | 'MTN' | 'Vodafone' | 'Safaricom';
-        Sequence: number;
-        ClientState?: any;
-    }
+  interface HubtelArgs {
+    Mobile: string;
+    SessionId: string;
+    ServiceCode: string;
+    Type: "Initiation" | "Response" | "Release" | "Timeout";
+    Message: string;
+    Operator: "Tigo" | "Airtel" | "MTN" | "Vodafone" | "Safaricom";
+    Sequence: number;
+    ClientState?: any;
+  }
 
-    type UssdMenuProvider = 'africasTalking' | 'hubtel';
-    interface UssdMenuOptions {
-        provider?: UssdMenuProvider;
-    }
-    
-    interface UssdStateOptions {
-        run(): void;
-        next?: NextState;
-        defaultNext?: string;
-    }
-    
-    interface UssdSessionConfig {
-        start(sessionId: string, callback?: Function): (Promise<any> | void);
-    
-        end(sessionId: string, callback?: Function): (Promise<any> | void);
-    
-        get(sessionId: string, key: string, callback?: Function): (Promise<any> | void);
-    
-        set(sessionId: string, key: string, value: any, callback?: Function): (Promise<any> | void);
-    }
+  type UssdMenuProvider = "africasTalking" | "hubtel";
+  interface UssdMenuOptions {
+    provider?: UssdMenuProvider;
+  }
+
+  interface UssdStateOptions {
+    run(): void;
+    next?: NextState;
+    defaultNext?: string;
+  }
+
+  interface UssdSessionConfig {
+    start(sessionId: string, callback?: Function): Promise<any> | void;
+
+    end(sessionId: string, callback?: Function): Promise<any> | void;
+
+    get(
+      sessionId: string,
+      key: string,
+      callback?: Function
+    ): Promise<any> | void;
+
+    set(
+      sessionId: string,
+      key: string,
+      value: any,
+      callback?: Function
+    ): Promise<any> | void;
+  }
 }
-
-
